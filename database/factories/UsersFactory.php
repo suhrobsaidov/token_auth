15623d62c6ec9aee01eb4c5ba3b7cfbc111158b8
suhrobsaidov <suhrob.aas@gmail.com>
2021-11-02 09:24:03 +0500

    first commit

---
+++ database/factories/UsersFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Users;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UsersFactory extends Factory
+{
+    protected $model = Users::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
